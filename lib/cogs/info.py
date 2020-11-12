687225f9a07b0e0e676de290c94a738cf10a4f2d
sorcerice <68538908+sorcerice@users.noreply.github.com>
2020-11-12 10:33:01 +0530

    testing help property of commands

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -81,7 +81,8 @@ class Info(Cog):
 
     @command(name='faq',
              description='To help out with support questions',
-             brief='To help out with support questions')
+             brief='To help out with support questions',
+             help='You need to have a valid faq tag\nCurrent FAQ tags are: ip, init, replay, shadow, helnif, aspd, enchants, resolution, boostermaps, 4th alt ticket, power dim essence, class rebalance')
     async def faq_command(self, ctx, *, faq_tag: str = 'list'):
         if faq_tag == 'ip':
             embed = Embed(title='FAQ',
