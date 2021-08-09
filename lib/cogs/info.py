007ac28aac4ed4a951945fe36a744d37d3944450
sorcerice <68538908+sorcerice@users.noreply.github.com>
2021-08-09 18:53:38 +0530

    added element table to  the faq tags

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -213,11 +213,14 @@ class Info(Cog):
         elif(faq_tag.lower() == 'class rebalance'):
             await ctx.send('https://www.divine-pride.net/forum/index.php?/topic/4203-kro-skill-adjustment-timeline/')
 
-        elif(faq_tag.lower() == 'booster char' or faq_tag.lower() == 'boosterchar'):
+        elif(faq_tag.lower() == any(['booster char', 'boosterchar'])):
             await ctx.send('`When you create a booster character, if you delete the  created  booster character or  if you cancel the booster NPC, **YOU WILL NOT BE ABLE TO CREATE A NEW BOOSTER CHARACTER** !`\nPlease pay attention and be careful when you are making your choices.')
 
+        elif(faq_tag.lower() == any(['element table', 'element tables', 'elements'])):
+            await ctx.send('https://cdn.discordapp.com/attachments/772453956810571829/874278146092982322/image0.png')
+
         else:
-            await ctx.send('You need to have a valid faq tag\nCurrent FAQ tags are: ip, init, replay, shadow, helnif, aspd, enchants, resolution, boostermaps, 4th alt ticket, power dim essence, class rebalance, booster char')
+            await ctx.send('You need to have a valid faq tag\nCurrent FAQ tags are: ip, init, replay, shadow, helnif, aspd, enchants, resolution, boostermaps, 4th alt ticket, power dim essence, class rebalance, booster char, elements')
 
     @Cog.listener()
     async def on_ready(self):
