536a0f64a6d1ea809a89afb6e91bb4fa07d78d78
sorcerice <68538908+sorcerice@users.noreply.github.com>
2021-09-22 15:34:10 +0530

    fixed faq helnif

--- lib/cogs/info.py
+++ lib/cogs/info.py
@@ -139,7 +139,8 @@ class Info(Cog):
         elif faq_tag.lower() == 'helnif' or faq_tag.lower() == 'nifhel':
             embed = Embed(title='Difference between Helheim and Niflheim',
                           description='''**TL;DR:**\n```**Helheim** = kRO only balance and progressions, newer server, more players.\n*``````*Niflheim** = A mix of kRO, jRO and iRO balance and progressions, older server, less players, more build possibilities, more competitively viable endgame builds, better endgame damage potentials due to gears and easier refines.```''',
-                          colour=0x4dfc32)
+                          colour=0x4dfc32,
+                          image='attachment://helnif.png')
 
             embed.set_author(name=ctx.author.display_name,
                              icon_url=ctx.author.avatar_url)
